496b46d0768238dee29c7d8c70f628a235396b24
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-11-27 00:10:55 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,18 @@
 # Textbooks
 # ===========================
 Textbook.create([
-  { id: 1, name: "音トレ道場6段", series: "音トレ", level: "英検準1級", cover_image: "ontre_06.png" },
-  { id: 2, name: "音トレ道場5段", series: "音トレ", level: "英検2級", cover_image: "ontre_05.png" },
-  { id: 3, name: "音トレ道場4段", series: "音トレ", level: "英検準2級", cover_image: "ontre_04.png" },
-  { id: 4, name: "音トレ道場3段", series: "音トレ", level: "英検3級", cover_image: "ontre_03.png" },
-  { id: 5, name: "音トレ道場2段", series: "音トレ", level: "英検4級", cover_image: "ontre_02.png" },
-  { id: 6, name: "音トレ道場初段", series: "音トレ", level: "英検5級", cover_image: "ontre_01.png" },
-  { id: 7, name: "リズムでマスター英検準1級", series: "リズムでマスター", level: "英検準1級", cover_image: "rhythm_06.png" },
-  { id: 8, name: "リズムでマスター英検2級", series: "リズムでマスター", level: "英検2級", cover_image: "rhythm_05.png" },
-  { id: 9, name: "リズムでマスター英検準2級", series: "リズムでマスター", level: "英検準2級", cover_image: "rhythm_04.png" },
-  { id: 10, name: "リズムでマスター英検3級", series: "リズムでマスター", level: "英検3級", cover_image: "rhythm_03.png" },
-  { id: 11, name: "リズムでマスター英検4級", series: "リズムでマスター", level: "英検4級", cover_image: "rhythm_02.png" },
-  { id: 12, name: "リズムでマスター英検5級", series: "リズムでマスター", level: "英検5級", cover_image: "rhythm_01.png" }
+  { id: 1, name: "音トレ道場6段", series: "音トレ", level: "英検準1級", cover_image: "ontre_06.jpg" },
+  { id: 2, name: "音トレ道場5段", series: "音トレ", level: "英検2級", cover_image: "ontre_05.jpg" },
+  { id: 3, name: "音トレ道場4段", series: "音トレ", level: "英検準2級", cover_image: "ontre_04.jpg" },
+  { id: 4, name: "音トレ道場3段", series: "音トレ", level: "英検3級", cover_image: "ontre_03.jpg" },
+  { id: 5, name: "音トレ道場2段", series: "音トレ", level: "英検4級", cover_image: "ontre_02.jpg" },
+  { id: 6, name: "音トレ道場初段", series: "音トレ", level: "英検5級", cover_image: "ontre_01.jpg" },
+  { id: 7, name: "リズムでマスター英検準1級", series: "リズムでマスター", level: "英検準1級", cover_image: "rhythm_06.jpg" },
+  { id: 8, name: "リズムでマスター英検2級", series: "リズムでマスター", level: "英検2級", cover_image: "rhythm_05.jpg" },
+  { id: 9, name: "リズムでマスター英検準2級", series: "リズムでマスター", level: "英検準2級", cover_image: "rhythm_04.jpg" },
+  { id: 10, name: "リズムでマスター英検3級", series: "リズムでマスター", level: "英検3級", cover_image: "rhythm_03.jpg" },
+  { id: 11, name: "リズムでマスター英検4級", series: "リズムでマスター", level: "英検4級", cover_image: "rhythm_02.jpg" },
+  { id: 12, name: "リズムでマスター英検5級", series: "リズムでマスター", level: "英検5級", cover_image: "rhythm_01.jpg" }
 ])
 
 # ===========================
