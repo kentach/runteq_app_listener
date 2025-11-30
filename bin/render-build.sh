d95780903683f1f14021a381f8bf27b500876077
Kenta <kenta@Kentas-MacBook-Pro.local>
2025-11-30 19:02:56 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,17 @@
+#!/usr/bin/env bash
 set -o errexit
 
+echo "Installing gems"
 bundle install
-bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
+echo "Running migrations"
 bundle exec rails db:migrate
+
+echo "Seeding database"
+bundle exec rails db:seed
+
+echo "Precompiling assets"
+bundle exec rails assets:precompile
+
+# 古いアセットを削除（必要なら）
+# bundle exec rails assets:clean
